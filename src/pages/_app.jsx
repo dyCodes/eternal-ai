a1bd761c42e50c736a5241715aabbebf7e7d63ea
dyCodes <yusufdaudu51@gmail.com>
2024-08-09 06:02:43 +0100

    chore: add & setup dependencies - axios, react-toastify

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,15 @@
-import { Layout } from "@/components";
-import "@/styles/globals.css";
+import { Layout } from '@/components';
+import '@/styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ToastContainer />
+    </>
   );
 }
